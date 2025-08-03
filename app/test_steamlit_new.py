ecaf06f1b7ef29994a3cd8e99e98e4549bc7e3ff
jagadeeshvarma927 <jagadeeshvarma927@gmail.com>
2025-08-03 13:44:29 +0530

    PPT generation commit

--- app/test_steamlit_new.py
+++ app/test_steamlit_new.py
@@ -107,6 +107,6 @@ elif selected == "View PPTs":
 # Footer
 st.markdown("""
 <div style="text-align: center; margin-top: 50px;">
-    <p>Made with ❤️ by Your Name</p>
+    <p>Made with ❤️ by Jagadeesh Varma I</p>
 </div>
 """, unsafe_allow_html=True)
